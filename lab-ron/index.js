ce45499252d0822806da052e376a54ad17f263bb
ron oh <ronoh2001@gmail.com>
2016-05-01 09:42:30 -0700

    new branch lab-02-ron

---
+++ lab-ron/index.js
@@ -0,0 +1,13 @@
+'use strict';
+//importing greet method from greetings.js
+const greet = require(__dirname + '/lib/greet').greet;
+
+//declare a const variable main
+//assign module.exports and main the value of a function
+const main = module.exports = function(){
+  //return the value of greet with the argv[2] as an argument
+  return greet(process.argv[2]);
+
+};
+//invoke console.log the result of invoking the main function
+console.log(main());
