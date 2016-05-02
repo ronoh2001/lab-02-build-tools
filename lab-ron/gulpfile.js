76f7c2deeb96bcc3f944f118ea0f4352d4fd20ac
ron oh <ronoh2001@gmail.com>
2016-05-01 21:15:16 -0700

    update gulfile and dependencies in package.json

---
+++ lab-ron/gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const gulp = require ('gulp');
+const eslint = require ('gulp-eslint');
+const mocha = require ('gulp-mocha');
+// const watch = require ('gulp-watch');
+
+gulp.task('mocha', function(){
+  return gulp.src('test/*.js').pipe(mocha());
+});
+gulp.task('eslint', function (){
+  return gulp.src(['index.js', 'lib/*.js', 'test/greet-test.js']).pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./test/*.js', ['default']);
+});
+// gulp.task('mocha', function(){
+//   return gulp.src(['index.js', 'lib/*.js]'])pipe.(mocha());
+//
+// })
+
+gulp.task('default', ['mocha', 'eslint', 'watch' ]);
