d9a50299f0e67551599ad286de27aaf7b5e7db1d
ron oh <ronoh2001@gmail.com>
2016-05-08 23:05:06 -0700

    update

--- lab-ron/test/greet-test.js
+++ lab-ron/test/greet-test.js
@@ -1,13 +1,26 @@
 'use strict';
 
-const greet = require(__dirname + '/../index.js');
+var main = require(__dirname + '/../index');
 const assert = require('assert');
 
-describe('testing module lab-ron/greet.js', function(){
-  describe('testing function greet()', function(){
-    it('should return hello + name', function(){
-      assert.equal(greet(), 'hello Ron Oh');
-      // assert.equal(greet('Yunjoo'), 'hello Yunjoo');
+describe('testing module index', function(){
+  describe('testing main function', function(){
+    it('process.argv[2] = should return "hello Ron Oh"', function(){
+      assert.equal(main(), 'hello Ron Oh');
+
     });
   });
 });
+// use strict';
+//
+// const greet = require(__dirname + '/../index.js');
+// const assert = require('assert');
+//
+// describe('testing module lab-ron/greet.js', function(){
+//   describe('testing function greet()', function(){
+//     it('should return hello + name', function(){
+//       assert.equal(greet(), 'hello Ron Oh');
+//       // assert.equal(greet('Yunjoo'), 'hello Yunjoo');
+//     });
+//   });
+// });
