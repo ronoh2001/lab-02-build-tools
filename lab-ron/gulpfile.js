f2c4f241af052e42464fc2c4f4d542c010a84d35
ron oh <ronoh2001@gmail.com>
2016-05-22 15:36:05 -0700

    update for submission

--- lab-ron/gulpfile.js
+++ lab-ron/gulpfile.js
@@ -3,18 +3,24 @@
 
 const gulp = require ('gulp');
 const mocha = require ('gulp-mocha');
+const eslint = require('gulp-eslint');
 // const watch = require ('gulp-watch');
 
 gulp.task('mocha', function(){
   return gulp.src('test/*.js').pipe(mocha());
 });
+gulp.task('eslint', function(){
+  return gulp.src(['*.js', '!node_modules/**']).pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
 
 gulp.task('watch', function(){
-  gulp.watch(['**/*.js', '!node_modules/**'], 'mocha');
+  gulp.watch(['**/*.js', '!node_modules/**'], ['mocha']);
 });
 // gulp.task('mocha', function(){
 //   return gulp.src(['index.js', 'lib/*.js]'])pipe.(mocha());
 //
 // })
 
-gulp.task('default', ['mocha', 'watch' ]);
+gulp.task('default', ['mocha', 'eslint', 'watch' ]);
